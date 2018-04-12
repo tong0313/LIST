4760e5bdff8a6a899b20dc2a7b867faca1b7a50c
tong <dsa0dsad..a>
2018-04-12 18:14:03 +0800

    LIST練習

---
+++ A106082024/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A106082024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.ReadLine();
+                // 1. 將所有號碼都放入List之中
+                List<int> numbers = new List<int>(); 
+
+                for(int i = 1; i<43; i++)
+                {
+                    numbers.Add(i); 
+                }
+
+                Console.ReadLine();
+                Random rand = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    
+                    // 2. 隨機挑選其中一個印出
+                    int r = rand.Next(1,numbers.Count);
+
+                    Console.WriteLine(numbersp[r] + "" );
+                    
+                    // 3. 並從 List 中刪除
+                    numbers.RemoveAt(r);
+                 }
+                   Console.ReadLine();  
+            }
+        }  
+    }
+}
---
+++ Random.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A106082024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i<6; i++)
+            {
+                //宣告 r0 到 r5 為隨機亂數 1~42
+                int r0 = rand.Next(1, 43);
+                int r1 = rand.Next(1, 43);
+                int r2 = rand.Next(1, 43);
+                int r3 = rand.Next(1, 43);
+                int r4 = rand.Next(1, 43);
+                int r5 = rand.Next(1, 43);
+                
+                //印出
+                Console.WriteLine("{0:00} {1:00} {2:00} {3:00} {4:00}" , r0 , r1 , r2 , r3 , r4 , r5 );
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
